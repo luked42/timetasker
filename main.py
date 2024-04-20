8093521a2919a9c6454afc2fff464cf52aafa7f6
Luke <luke@Lukes-MacBook-Pro.local>
2024-04-20 11:29:30 +0200

    adding complete counter

--- main.py
+++ main.py
@@ -2,23 +2,56 @@ from time import monotonic
 
 from textual.app import App, ComposeResult
 from textual.reactive import reactive
-from textual.widgets import Footer, Digits
+from textual.widgets import Digits, Static
 
 
-class TimeDisplay(Digits):
-    start_time: reactive[float] = reactive(monotonic)
-    total_countdown_seconds: reactive[float] = reactive(25.0 * 60)
-    time_left_seconds: reactive[float] = reactive(25.0 * 60)
+class CompleteCounter(Static):
+    complete_count: reactive[int] = reactive(0)
 
     def __init__(self, id: str) -> None:
         super().__init__(id=id)
+
+    def on_mount(self) -> None:
+        self._update_count()
+
+    def increment_count(self) -> None:
+        self.complete_count += 1
+        self._update_count()
+
+    def _update_count(self) -> None:
+        self.update(f"{self.complete_count}")
+
+
+class FooterBar(Static):
+    def __init__(self, id: str, complete_counter: CompleteCounter) -> None:
+        super().__init__(id=id)
+        self.complete_counter = complete_counter
+
+    def compose(self) -> ComposeResult:
+        yield self.complete_counter
+
+
+class TimeDisplay(Digits):
+    minutes: float = 0.05
+    start_time: reactive[float] = reactive(monotonic)
+    total_countdown_seconds: reactive[float] = reactive(0)
+    time_left_seconds: reactive[float] = reactive(0)
+
+    def __init__(self, id: str, complete_counter: CompleteCounter) -> None:
+        super().__init__(id=id)
         self.started = False
+        self.finished = False
+        self.complete_counter: CompleteCounter = complete_counter
 
     def on_mount(self) -> None:
         self.update_timer = self.set_interval(1, self.update_time, pause=True)
+        self.reset()
 
     def update_time(self) -> None:
-        self.time_left_seconds = self.total_countdown_seconds - (monotonic() - self.start_time)
+        self.time_left_seconds = max(0.0, self.total_countdown_seconds - (monotonic() - self.start_time))
+        if self.time_left_seconds <= 0.0 and not self.finished:
+            self.complete_counter.increment_count()
+            self.finished = True
 
     def watch_time_left_seconds(self, time: float) -> None:
         minutes, seconds = divmod(time, 60)
@@ -39,13 +72,15 @@ class TimeDisplay(Digits):
     def reset(self):
         self.stop()
         self.start_time = monotonic()
-        self.time_left_seconds = 25.0 * 60
+        self.time_left_seconds = self.minutes * 60
+        self.finished = False
 
     def toggle_timer(self):
         if self.started:
             self.stop()
         else:
             self.start()
+
 
 class PomodoroTimer(App):
     """A Textual app to manage stopwatches."""
@@ -58,13 +93,13 @@ class PomodoroTimer(App):
     ]
 
     def compose(self) -> ComposeResult:
-        yield TimeDisplay(id="countdown_timer")
-        yield Footer()
+        complete_counter: CompleteCounter = CompleteCounter(id="complete_counter")
+        yield TimeDisplay(id="countdown_timer", complete_counter=complete_counter)
+        yield FooterBar(id="footer_bar", complete_counter=complete_counter)
 
     def action_toggle_timer(self) -> None:
         countdown_timer: TimeDisplay = self.query_one(TimeDisplay)
         countdown_timer.toggle_timer()
-
 
     def action_reset_timer(self) -> None:
         countdown_timer: TimeDisplay = self.query_one(TimeDisplay)
